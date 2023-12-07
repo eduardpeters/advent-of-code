6c112d912e075459749f8149d550868d088f2e22
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-12-07 18:20:26 +0100

    Setup day 7

---
+++ 2023/day_7/main.py
@@ -0,0 +1,27 @@
+def solve(path, part):
+    if part == 1:
+        sum = 0
+    else:
+        sum = 1
+    print(sum)
+
+
+def choose_action():
+    choice = int(
+        input('0. Example One\n1. Part One\n2. Example Two\n3. Part Two\n-> '))
+    if choice == 0:
+        print('Running example part 1')
+        solve('example.txt', 1)
+    elif choice == 1:
+        print('Running input part 1')
+        solve('input.txt', 1)
+    elif choice == 2:
+        print('Running example part 2')
+        solve('example.txt', 2)
+    else:
+        print('Running input part 2')
+        solve('input.txt', 2)
+
+
+if __name__ == "__main__":
+    choose_action()

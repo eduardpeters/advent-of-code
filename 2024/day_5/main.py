72a87c70e60a899d34aab6be4f1084110de5a3b0
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-12-08 14:08:27 +0100

    Solve part two

--- 2024/day_5/main.py
+++ 2024/day_5/main.py
@@ -1,21 +1,31 @@
 def solve(path: str, part: int) -> None:
     rules, updates = load_file(path)
     print(rules)
-    print(updates)
+    correct_updates, incorrect_updates = get_classified_updates(rules, updates)
     if part == 1:
         print("Solving for part 1")
-        correct_updates = get_correct_updates(rules, updates)
         print(f"Correct updates: {correct_updates}")
-        middle_page_sum = sum([update[len(update) // 2] for update in correct_updates])
-        print(f"Middle page sum: {middle_page_sum}")
+        middle_page_sum = get_middle_page_sum(correct_updates)
     else:
         print("Solving for part 2")
+        print(f"Incorrect updates:")
+        print(incorrect_updates)
+        corrected_updates = amend_incorrect_updates(rules, incorrect_updates)
+        print("Corrected updates")
+        print(corrected_updates)
+        middle_page_sum = get_middle_page_sum(corrected_updates)
+    print(f"Middle page sum: {middle_page_sum}")
 
 
-def get_correct_updates(
+def get_middle_page_sum(updates: list[list[int]]) -> int:
+    return sum([update[len(update) // 2] for update in updates])
+
+
+def get_classified_updates(
     rules: dict[int, list[int]], updates: list[list[int]]
-) -> list[list[int]]:
+) -> tuple[list[list[int]], list[list[int]]]:
     correct_updates: list[list[int]] = []
+    incorrect_updates: list[list[int]] = []
     for update in updates:
         for i in range(1, len(update)):
             current_page = update[i]
@@ -24,6 +34,7 @@ def get_correct_updates(
             for j in range(i - 1, -1, -1):
                 preceding_page = update[j]
                 if preceding_page in rules[current_page]:
+                    incorrect_updates.append(update)
                     break
             else:
                 continue  # for loop found no invalid numbers, check next number
@@ -32,7 +43,34 @@ def get_correct_updates(
             # only if inner loop did not break is the update valid
             correct_updates.append(update)
 
-    return correct_updates
+    return correct_updates, incorrect_updates
+
+
+def amend_incorrect_updates(
+    rules: dict[int, list[int]], incorrect_updates: list[list[int]]
+) -> list[list[int]]:
+    corrected_updates: list[list[int]] = []
+    for incorrect_update in incorrect_updates:
+        corrected_update: list[int] = incorrect_update
+        i: int = 1
+        while i < len(corrected_update):
+            current_page = corrected_update[i]
+            if current_page not in rules:
+                i += 1
+                continue
+            j: int = i - 1
+            while j > -1:
+                preceding_page = corrected_update[j]
+                if preceding_page in rules[current_page]:
+                    page_to_move = corrected_update.pop(j)
+                    corrected_update.append(page_to_move)
+                    # Update current page index to account for list shift
+                    i -= 1
+                j -= 1
+            i += 1
+        corrected_updates.append(corrected_update)
+
+    return corrected_updates
 
 
 def load_file(path: str):

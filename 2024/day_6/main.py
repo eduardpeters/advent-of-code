0f642d7aea22e2f8418bafe65b0c8590b2e8d2ef
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-12-09 21:12:58 +0100

    Solve part 1

---
+++ 2024/day_6/main.py
@@ -0,0 +1,124 @@
+from enum import Enum
+
+GUARD = "^"
+OBSTRUCTION = "#"
+VISITED = "X"
+
+
+class Direction(Enum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+
+def solve(path: str, part: int) -> None:
+    area = load_file(path)
+    for row in area:
+        print(row)
+    if part == 1:
+        print("Solving for part 1")
+        starting_position = get_starting_position(area)
+        starting_direction = Direction.UP
+        print(starting_position)
+        positions_visited = get_patrolled_positions(
+            area, starting_position, starting_direction
+        )
+        print(f"Guard visited: {positions_visited} positions")
+    else:
+        print("Solving for part 2")
+
+
+def get_patrolled_positions(
+    area: list[list[str]],
+    starting_position: tuple[int, int],
+    starting_direction: Direction,
+) -> int:
+    visited = 1
+
+    current_position = starting_position
+    current_direction = starting_direction
+    while current_position[0] < len(area) and current_position[1] < len(
+        area[current_position[0]]
+    ):
+        next_position = move_forward(current_position, current_direction)
+        if next_position[0] >= len(area) or next_position[1] >= len(
+            area[next_position[0]]
+        ):
+            # Guard has stepped out of bounds
+            return visited
+        next_position_content = area[next_position[0]][next_position[1]]
+        if next_position_content == OBSTRUCTION:
+            current_direction = turn_right(current_direction)
+        else:
+            # Only count if not visited yet and mark it
+            if area[current_position[0]][current_position[1]] != VISITED:
+                visited += 1
+                area[current_position[0]][current_position[1]] = VISITED
+            current_position = next_position
+
+    return visited
+
+
+def turn_right(current_direction: Direction) -> Direction:
+    match current_direction:
+        case Direction.UP:
+            return Direction.RIGHT
+        case Direction.RIGHT:
+            return Direction.DOWN
+        case Direction.DOWN:
+            return Direction.LEFT
+        case Direction.LEFT:
+            return Direction.UP
+
+
+def move_forward(
+    current_position: tuple[int, int], current_direction: Direction
+) -> tuple[int, int]:
+    match current_direction:
+        case Direction.UP:
+            return (current_position[0] - 1, current_position[1])
+        case Direction.DOWN:
+            return (current_position[0] + 1, current_position[1])
+        case Direction.RIGHT:
+            return (current_position[0], current_position[1] + 1)
+        case Direction.LEFT:
+            return (current_position[0], current_position[1] - 1)
+
+
+def get_starting_position(area: list[list[str]]) -> tuple[int, int]:
+    for row in range(0, len(area)):
+        for column, position in enumerate(area[row]):
+            if position == GUARD:
+                return row, column
+    return (0, 0)
+
+
+def load_file(path: str) -> list[list[str]]:
+    area: list[list[str]] = []
+
+    with open(path) as file:
+        for line in file:
+            trimmed_line = line.strip()
+            area.append(list(trimmed_line))
+    return area
+
+
+def choose_action():
+    choice = int(input("0. Example One\n1. Part One\n2. Example Two\n3. Part Two\n-> "))
+    if choice == 0:
+        print("Running example part 1")
+        solve("example.txt", 1)
+    elif choice == 1:
+        print("Running input part 1")
+        solve("input.txt", 1)
+    elif choice == 2:
+        print("Running example part 2")
+        solve("example.txt", 2)
+    else:
+        print("Running input part 2")
+        solve("input.txt", 2)
+
+
+if __name__ == "__main__":
+    choose_action()

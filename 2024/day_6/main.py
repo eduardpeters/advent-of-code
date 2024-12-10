6c347ebe908ae44ed455e4b37ec7a0d59c0738a5
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2024-12-10 19:50:21 +0100

    Solve part 2

--- 2024/day_6/main.py
+++ 2024/day_6/main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+EMPTY = "."
 GUARD = "^"
 OBSTRUCTION = "#"
 VISITED = "X"
@@ -16,10 +17,10 @@ def solve(path: str, part: int) -> None:
     area = load_file(path)
     for row in area:
         print(row)
+    starting_position = get_starting_position(area)
+    starting_direction = Direction.UP
     if part == 1:
         print("Solving for part 1")
-        starting_position = get_starting_position(area)
-        starting_direction = Direction.UP
         print(starting_position)
         positions_visited = get_patrolled_positions(
             area, starting_position, starting_direction
@@ -27,6 +28,75 @@ def solve(path: str, part: int) -> None:
         print(f"Guard visited: {positions_visited} positions")
     else:
         print("Solving for part 2")
+        new_obstruction_positions = get_new_obstruction_positions(
+            area, starting_position, starting_direction
+        )
+        print(f"Found {len(new_obstruction_positions)} positions that create loops")
+
+
+def get_new_obstruction_positions(
+    area: list[list[str]],
+    starting_position: tuple[int, int],
+    starting_direction: Direction,
+) -> list[tuple[int, int]]:
+    obstruction_positions: list[tuple[int, int]] = []
+
+    for row in range(0, len(area)):
+        for column in range(0, len(area[row])):
+            position_content = area[row][column]
+            if position_content != EMPTY:
+                continue
+            area[row][column] = OBSTRUCTION
+            if is_in_a_loop(area, starting_position, starting_direction):
+                obstruction_positions.append((row, column))
+            area[row][column] = EMPTY
+            print(
+                f"Checked: ({row},{column}) new positions: {len(obstruction_positions)}"
+            )
+
+    return obstruction_positions
+
+
+def is_in_a_loop(
+    area: list[list[str]],
+    starting_position: tuple[int, int],
+    starting_direction: Direction,
+) -> bool:
+    visit_history: dict[tuple[int, int], list[Direction]] = {}
+
+    current_position = starting_position
+    current_direction = starting_direction
+    while 0 <= current_position[0] < len(area) and 0 <= current_position[1] < len(
+        area[current_position[0]]
+    ):
+        if current_position in visit_history:
+            visit_history[current_position].append(current_direction)
+        else:
+            visit_history[current_position] = [current_direction]
+
+        next_position = move_forward(current_position, current_direction)
+        if (
+            next_position[0] < 0
+            or next_position[0] >= len(area)
+            or next_position[1] < 0
+            or next_position[1] >= len(area[next_position[0]])
+        ):
+            # Guard has stepped out of bounds, no loop
+            return False
+        if (
+            next_position in visit_history
+            and current_direction in visit_history[next_position]
+        ):
+            # Guard has walked this path before, is a loop
+            return True
+
+        next_position_content = area[next_position[0]][next_position[1]]
+        if next_position_content == OBSTRUCTION:
+            current_direction = turn_right(current_direction)
+        else:
+            current_position = next_position
+    # Guard has stepped out of bounds, no loop
+    return False
 
 
 def get_patrolled_positions(
@@ -38,12 +108,15 @@ def get_patrolled_positions(
 
     current_position = starting_position
     current_direction = starting_direction
-    while current_position[0] < len(area) and current_position[1] < len(
+    while 0 <= current_position[0] < len(area) and 0 <= current_position[1] < len(
         area[current_position[0]]
     ):
         next_position = move_forward(current_position, current_direction)
-        if next_position[0] >= len(area) or next_position[1] >= len(
-            area[next_position[0]]
+        if (
+            next_position[0] < 0
+            or next_position[0] >= len(area)
+            or next_position[1] < 0
+            or next_position[1] >= len(area[next_position[0]])
         ):
             # Guard has stepped out of bounds
             return visited

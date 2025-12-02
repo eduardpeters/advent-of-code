dbbb3ee870865dea2cfd7110614370dfe4a40c55
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-12-02 18:34:29 +0100

    solve part 2

--- 2025/day_1/main.py
+++ 2025/day_1/main.py
@@ -6,40 +6,66 @@ DIRECTION_LEFT = "L"
 DIRECTION_RIGHT = "R"
 
 
-def apply_rotation(current_position: int, rotation: str) -> int:
+def get_rotation_components(rotation: str) -> tuple[str, int]:
     direction = rotation[0]
     magnitude = int(rotation[1:])
+    return (direction, magnitude)
+
+
+def apply_rotation(current_position: int, rotation: str) -> tuple[int, int]:
+    direction, magnitude = get_rotation_components(rotation)
+    zero_clicks = 0
     print("Rotating", direction, magnitude)
-    # Full rotations
-    while magnitude >= MAX_POSITION + 1:
-        magnitude -= MAX_POSITION + 1
 
-    if direction == DIRECTION_RIGHT:
-        return (current_position + magnitude) % (MAX_POSITION + 1)
-    if magnitude > current_position:
-        # Go to 99
-        magnitude -= current_position + 1
-        current_position = MAX_POSITION
+    if direction == DIRECTION_LEFT:
+        movements = -magnitude
+        movements_to_zero = current_position
+    else:
+        movements = magnitude
+        if current_position == 0:
+            movements_to_zero = 0
+        else:
+            movements_to_zero = MAX_POSITION + 1 - current_position
 
-    return current_position - magnitude
+    tmp = current_position + movements
+    if magnitude >= movements_to_zero:
+        if current_position != 0:
+            zero_clicks += 1
+        zero_clicks += (magnitude - movements_to_zero) // (MAX_POSITION + 1)
+    final_position = tmp % (MAX_POSITION + 1)
+
+    return final_position, zero_clicks
 
 
 def solve(path: str, part: int) -> None:
     rotations = load_file(path)
-    print(rotations)
+
     if part == 1:
         print("Solving for part 1")
         zero_count = 0
         position = START_POSITION
         for rotation in rotations:
             print(f"Before position: {position}")
-            position = apply_rotation(position, rotation)
+            position, _ = apply_rotation(position, rotation)
             print(f"After position: {position}")
             if position == 0:
                 zero_count += 1
         print(f"Zero count: {zero_count}")
     else:
         print("Solving for part 2")
+        zero_count = 0
+        zero_clicks = 0
+        position = START_POSITION
+        for rotation in rotations:
+            print(f"Before position: {position}")
+            position, clicks = apply_rotation(position, rotation)
+            print(f"After position: {position}")
+            if position == 0:
+                zero_count += 1
+            zero_clicks += clicks
+        print(f"Zero count: {zero_count}")
+        print(f"Zero clicks: {zero_clicks}")
+        print(f"Total: {zero_count + zero_clicks}")
 
 
 def load_file(path: str) -> list[str]:

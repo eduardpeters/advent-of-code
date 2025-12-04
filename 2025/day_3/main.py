f48208c71fb7dba86dbc11a04b0dd8d0825f6e35
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-12-04 22:09:08 +0100

    solve part two

--- 2025/day_3/main.py
+++ 2025/day_3/main.py
@@ -16,17 +16,37 @@ def get_bank_joltage_pair(bank: str) -> int:
     return int(bank[high_h_idx] + bank[high_l_idx])
 
 
+def get_bank_joltage_dozen(bank: str) -> int:
+    batteries = [int(b) for b in bank]
+    indexes: list[int] = [i for i in range(12)]
+
+    current_index = 0
+    while current_index < len(indexes):
+        i = indexes[current_index] + 1
+        while i < (len(batteries) - len(indexes) + current_index + 1):
+            if batteries[i] > batteries[indexes[current_index]]:
+                for idx in range(len(indexes[current_index:])):
+                    indexes[current_index + idx] = i + idx
+            i += 1
+        current_index += 1
+
+    return int("".join([str(batteries[idx]) for idx in indexes]))
+
+
 def solve(path: str, part: int) -> None:
     banks = load_file(path)
 
+    joltage = 0
     if part == 1:
-        joltage = 0
         print("Solving for part 1")
         for bank in banks:
             joltage += get_bank_joltage_pair(bank)
-        print(f"Joltage: {joltage}")
     else:
         print("Solving for part 2")
+        for bank in banks:
+            joltage += get_bank_joltage_dozen(bank)
+
+    print(f"Joltage: {joltage}")
 
 
 def load_file(path: str) -> list[str]:

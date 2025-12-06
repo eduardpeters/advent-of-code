5302b8b4f6364e32edd0f34552af1aade6ae1fec
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-12-06 22:25:12 +0100

    solve part two

--- 2025/day_6/main.py
+++ 2025/day_6/main.py
@@ -2,11 +2,18 @@ ADD = "+"
 MULTIPLY = "*"
 
 
-def solve(path: str, part: int) -> None:
-    numbers, operators = load_file(path)
+def load_raw_file_reversed(path: str) -> list[str]:
+    lines: list[str] = []
+    with open(path) as file:
+        for line in file:
+            lines.append(line.removesuffix("\n")[::-1])
+    return lines
 
+
+def solve(path: str, part: int) -> None:
     if part == 1:
         print("Solving for part 1")
+        numbers, operators = load_file(path)
         grand_total = 0
         problem = 0
         while problem < len(numbers):
@@ -24,6 +31,45 @@ def solve(path: str, part: int) -> None:
         print(f"Grand Total: {grand_total}")
     else:
         print("Solving for part 2")
+        lines = load_raw_file_reversed(path)
+        numbers: list[list[int]] = []
+        operators: list[str] = []
+        # Initialize an array for each possible problem set
+        for _ in range(len(lines[0].split())):
+            numbers.append([])
+        # Extract operators
+        for op in lines[-1].strip().split():
+            operators.append(op)
+        problem_idx = 0
+        n_columns = len(lines[0])
+        for c in range(n_columns):
+            column_has_number = False
+            current_number = 0
+            for line in lines[:-1]:
+                current_char = line[c]
+                if current_char == " ":
+                    continue
+                column_has_number = True
+                current_number = current_number * 10 + int(current_char)
+            if column_has_number:
+                numbers[problem_idx].append(current_number)
+            else:
+                problem_idx += 1
+        grand_total = 0
+        problem_idx = 0
+        while problem_idx < len(numbers):
+            op = operators[problem_idx]
+            result = 0 if op == ADD else 1
+            for number in numbers[problem_idx]:
+                if op == ADD:
+                    result += number
+                else:
+                    result *= number
+
+            grand_total += result
+            problem_idx += 1
+
+        print(f"Grand Total: {grand_total}")
 
 
 def load_file(path: str) -> tuple[list[list[int]], list[str]]:

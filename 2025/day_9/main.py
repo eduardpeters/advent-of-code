1771e55a1d74b83a3509b6abe8d834124afc7f5a
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-12-10 22:06:25 +0100

    wip part two

--- 2025/day_9/main.py
+++ 2025/day_9/main.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Node:
+    position: tuple[int, int]
+    next: "Node | None" = None
+    prev: "Node | None" = None
+
+
 def side_length(a: int, b: int) -> int:
     return abs(a - b) + 1
 
@@ -6,12 +16,51 @@ def rectangle_area(a: tuple[int, int], b: tuple[int, int]) -> int:
     return side_length(a[0], b[0]) * side_length(a[1], b[1])
 
 
+def is_on_perimeter(perimeter: Node, x: tuple[int, int]) -> bool:
+    if not perimeter.next:
+        return False
+    a = perimeter.position
+    b = perimeter.next.position
+    if a[0] == b[0] and a[0] == x[0]:
+        if a[1] < x[1] < b[1] or a[1] > x[1] > b[1]:
+            return True
+    if a[1] == b[1] and a[1] == x[1]:
+        if a[0] < x[0] < b[0] or a[0] > x[0] > b[0]:
+            return True
+    return False
+
+
+def build_perimeter(tiles: list[tuple[int, int]]) -> Node:
+    base_node = Node(position=tiles[0])
+    current_node = base_node
+    for tile in tiles[1:]:
+        current_node.next = Node(position=tile, prev=current_node)
+        current_node = current_node.next
+
+    current_node.next = base_node
+    base_node.prev = current_node
+
+    return base_node
+
+
+def get_final_rectangle_vertex(
+    a: tuple[int, int], b: tuple[int, int], c: tuple[int, int]
+) -> tuple[int, int]:
+    vertex_position: tuple[int, int] = (0, 0)
+    if a[0] == b[0]:
+        vertex_position = (c[0], a[1])
+    else:
+        vertex_position = (a[0], c[1])
+
+    return vertex_position
+
+
 def solve(path: str, part: int) -> None:
     tiles = load_file(path)
 
+    max_area = 1
     if part == 1:
         print("Solving for part 1")
-        max_area = 0
         for i, pivot in enumerate(tiles):
             for opposite in tiles[i + 1 :]:
                 area = rectangle_area(pivot, opposite)
@@ -20,6 +69,18 @@ def solve(path: str, part: int) -> None:
         print(f"Max area found: {max_area}")
     else:
         print("Solving for part 2")
+        base_node = build_perimeter(tiles)
+        current_node = base_node
+        count = 0
+        while current_node and current_node.next:
+            count += 1
+            # Setup for next iteration
+            current_node = current_node.next
+            if current_node.position == base_node.position:
+                break
+        print(f"Nodes: {count}")
+
+    print(f"Max area found: {max_area}")
 
 
 def load_file(path: str) -> list[tuple[int, int]]:

2b0e9cd731b9b66dae032b7846553d869d018135
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-12-13 22:53:51 +0100

    wip gaussian elimination and bounds

--- 2025/day_10/main.py
+++ 2025/day_10/main.py
@@ -1,3 +1,6 @@
+import copy
+import numpy as np
+
 ON = 1
 OFF = 0
 
@@ -24,13 +27,6 @@ class MachineConfiguration:
                     self.joltage.append(int(joltage))
 
 
-def lights_equal(a: list[int], b: list[int]) -> bool:
-    for light_a, light_b in zip(a, b):
-        if light_a != light_b:
-            return False
-    return True
-
-
 def press_button(lights: list[int], button: list[int]) -> list[int]:
     for toggle in button:
         lights[toggle] = ON if lights[toggle] == OFF else OFF
@@ -49,12 +45,171 @@ def toggle_indicator_lights(goal: list[int], buttons: list[list[int]]) -> int:
                 if last_button_idx == idx:
                     continue
                 new_state = press_button(state[:], button)
-                if lights_equal(goal, new_state):
+                if goal == new_state:
                     return presses
                 new_queue.append((new_state, idx))
         queue = new_queue
 
     return presses
+
+
+def print_matrix(matrix: list[np.ndarray], title: str = ""):
+    print(title)
+    for row in matrix:
+        print(row)
+
+
+def apply_joltage(joltage: list[int], button: list[int]) -> list[int]:
+    for idx in button:
+        joltage[idx] += 1
+    return joltage
+
+
+def has_overjoltage(goal: list[int], joltage: list[int]) -> bool:
+    for goal_joltage, current_joltage in zip(goal, joltage):
+        if current_joltage > goal_joltage:
+            return True
+    return False
+
+
+def build_matrix(goal: list[int], buttons: list[list[int]]) -> list[np.ndarray]:
+    equations = [np.array([0 for _ in buttons]) for _ in goal]
+    for e_idx, equation in enumerate(equations):
+        for b_idx, button in enumerate(buttons):
+            for idx in button:
+                if idx != e_idx:
+                    continue
+                equation[b_idx] = 1
+    result = np.array(goal)
+    matrix: list[np.ndarray] = []
+    for equation, result in zip(equations, result):
+        row = np.append(equation, result)
+        matrix.append(row)
+
+    return matrix
+
+
+def matrix_elimination(matrix: list[np.ndarray]) -> list[np.ndarray]:
+    print_matrix(matrix, "Matrix:")
+    for idx in range(min(len(matrix), len(matrix[0]))):
+        if matrix[idx][idx] == 0:
+            best_swap_row = -1
+            for row in range(idx + 1, len(matrix)):
+                if matrix[row][idx] != 0:
+                    if matrix[row][idx] == 1:
+                        best_swap_row = row
+                    elif matrix[row][idx] > 0:
+                        if best_swap_row < 0:
+                            best_swap_row = row
+                        elif matrix[row][idx] < matrix[best_swap_row][idx]:
+                            best_swap_row = row
+            tmp = matrix[best_swap_row]
+            matrix[best_swap_row] = matrix[idx]
+            matrix[idx] = tmp
+        elif matrix[idx][idx] != 0:
+            factor = 1 // matrix[idx][idx]
+            matrix[idx] = factor * matrix[idx]
+        for row in range(idx + 1, len(matrix)):
+            if matrix[row][idx] != 0:
+                factor = matrix[row][idx] // matrix[idx][idx]
+                matrix[row] -= factor * matrix[idx]
+
+    print_matrix(matrix, "After Elimination:")
+    return matrix
+
+
+def count_variables(equation: np.ndarray) -> int:
+    count = 0
+    for var in equation[:-1]:
+        if var != 0:
+            count += 1
+    return count
+
+
+def solve_single_variable(equation: np.ndarray) -> tuple[int, tuple[int, int]] | None:
+    idx_coefficient = -1
+    for coeff in equation[:-1]:
+        idx_coefficient += 1
+        if coeff != 0:
+            break
+    if equation[-1] % equation[idx_coefficient] != 0:
+        return None
+    result = equation[-1] // equation[idx_coefficient]
+    return idx_coefficient, (result.item(), result.item())
+
+
+def constrain_upper_bounds_by_equation(
+    equation: np.ndarray, coefficients: list[tuple[int, int]], seen_indeces: set[int]
+) -> list[tuple[int, tuple[int, int]]]:
+    updated: list[tuple[int, tuple[int, int]]] = []
+
+    equation_coefficients: list[int] = [
+        idx for idx, value in enumerate(equation[:-1]) if value != 0
+    ]
+    idx_pivot = 0
+    while idx_pivot < len(equation) - 1:
+        coefficient_pivot = equation[idx_pivot]
+        if coefficient_pivot == 0 or idx_pivot in seen_indeces:
+            idx_pivot += 1
+            continue
+        pivot_upper_bound = equation[-1]
+        for idx_coefficient in equation_coefficients:
+            if idx_coefficient == idx_pivot:
+                continue
+            value_coefficient = equation[idx_coefficient]
+            if value_coefficient < 0:
+                coefficient_upper_bound = coefficients[idx_coefficient][1]
+                coefficient_bound_factor = (
+                    coefficient_upper_bound if coefficient_upper_bound != -1 else 0
+                )
+            else:
+                coefficient_bound_factor = coefficients[idx_coefficient][0]
+
+            pivot_upper_bound -= equation[idx_coefficient] * coefficient_bound_factor
+        if pivot_upper_bound % coefficient_pivot == 0:
+            seen_indeces.add(idx_pivot)
+            pivot_upper_bound = pivot_upper_bound // coefficient_pivot
+            updated.append(
+                (idx_pivot, (coefficients[idx_pivot][0], abs(pivot_upper_bound.item())))
+            )
+        idx_pivot += 1
+
+    return updated
+
+
+def constrain_coefficients(
+    matrix: list[np.ndarray], coefficients: list[tuple[int, int]]
+) -> list[tuple[int, int]]:
+    seen: set[int] = set()
+    for row in matrix[::-1]:
+        variables = count_variables(row)
+        if variables <= 0:
+            continue
+        if variables == 1:
+            solved = solve_single_variable(row)
+            if not solved:
+                continue
+            coefficients[solved[0]] = solved[1]
+            seen.add(solved[0])
+        else:
+            coefficient_updates = constrain_upper_bounds_by_equation(
+                row, coefficients, seen
+            )
+            for idx, bounds in coefficient_updates:
+                coefficients[idx] = bounds
+
+    return coefficients
+
+
+def raise_joltage(goal: list[int], buttons: list[list[int]]) -> int:
+    print(f"Solving for: {goal} using {len(buttons)} buttons")
+    matrix = build_matrix(goal, buttons)
+    reduced_matrix = matrix_elimination(copy.deepcopy(matrix))
+    coefficients: list[tuple[int, int]] = [(0, -1) for _ in matrix[0][:-1]]
+    coefficients = constrain_coefficients(reduced_matrix, coefficients)
+    for idx, coef in enumerate(coefficients):
+        print(f"Button: {idx + 1} Bounds: {coef}")
+    return 0
 
 
 def solve(path: str, part: int) -> None:
@@ -63,15 +218,19 @@ def solve(path: str, part: int) -> None:
     for config in config_lines:
         machines.append(MachineConfiguration(config))
 
+    press_count = 0
     if part == 1:
         print("Solving for part 1")
-        press_count = 0
         for machine in machines:
             press_count += toggle_indicator_lights(machine.lights, machine.buttons)
 
-        print(f"Button presses: {press_count}")
     else:
         print("Solving for part 2")
+        for machine in machines:
+            press_count += raise_joltage(machine.joltage, machine.buttons)
+            break
+
+    print(f"Button presses: {press_count}")
 
 
 def load_file(path: str) -> list[str]:
